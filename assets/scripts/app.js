b06f1b2a5a06b60e8029c72d0839d58e16e76966
Alojzy Klim <adklim@gmail.com>
2020-11-13 14:49:22 -0600

    added functionality to operator buttons

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,10 +1,42 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+function getUserInput() {
+   return parseInt(usrInput.value);
+}
+
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+    outputResult(currentResult, calcDescription);
+}
+
 function add() {
-    currentResult = currentResult + userInput.value;
-    outputResult(currentResult, '');
-   
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult + enteredNumber;
+    createAndWriteOutput('+', initialResult. enteredNumber);
+}
+
+function subtract() {
+    const enteredNumber = parseInt(usrInput.value);
+    const initialResult = currentResult;
+    currentResult = currentResult - enteredNumber;
+    createAndWriteOutput('-', initialResult. enteredNumber);
+}
+
+function multiply() {const enteredNumber = parseInt(usrInput.value);
+    const initialResult = currentResult;
+    currentResult = currentResult * enteredNumber;
+    createAndWriteOutput('*', initialResult. enteredNumber);
+}
+
+function divide() {const enteredNumber = parseInt(usrInput.value);
+    const initialResult = currentResult;
+    currentResult = currentResult / enteredNumber;
+    createAndWriteOutput('/', initialResult. enteredNumber);
 }
 
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click' , subtract);
+multiplyBtn.addEventListener('click' , multiply);
+divideBtn.addEventListener('click', divide);
