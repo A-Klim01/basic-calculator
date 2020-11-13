1375b680025f92cbbc32150635bca95b67629a64
Alojzy Klim <adklim@gmail.com>
2020-11-13 15:53:53 -0600

    added more code

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,38 +1,50 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
+let logEntries = [];
 
-function getUserInput() {
+// Gets input from input field
+function getUserNumberInput() {
    return parseInt(usrInput.value);
 }
 
+// Generates and Writes calculation log
 function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
     const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
-    outputResult(currentResult, calcDescription);
+    outputResult(currentResult, calcDescription); // from Vendor file
 }
 
 function add() {
     const enteredNumber = getUserNumberInput();
     const initialResult = currentResult;
-    currentResult = currentResult + enteredNumber;
-    createAndWriteOutput('+', initialResult. enteredNumber);
+    currentResult += enteredNumber;
+    createAndWriteOutput('+', initialResult.enteredNumber);
+    const logEntry = {
+        operation: 'ADD',
+        prevResult: initialResult,
+        number: enteredNumber,
+        result: currentResult
+    };
+    logEntries.push(logEntry);
+    console.log(logEntry);
+    console.log(logEntries[0]);
 }
 
 function subtract() {
     const enteredNumber = parseInt(usrInput.value);
     const initialResult = currentResult;
-    currentResult = currentResult - enteredNumber;
+    currentResult -= enteredNumber;
     createAndWriteOutput('-', initialResult. enteredNumber);
 }
 
 function multiply() {const enteredNumber = parseInt(usrInput.value);
     const initialResult = currentResult;
-    currentResult = currentResult * enteredNumber;
+    currentResult *= enteredNumber;
     createAndWriteOutput('*', initialResult. enteredNumber);
 }
 
 function divide() {const enteredNumber = parseInt(usrInput.value);
     const initialResult = currentResult;
-    currentResult = currentResult / enteredNumber;
+    currentResult /= enteredNumber;
     createAndWriteOutput('/', initialResult. enteredNumber);
 }
 
